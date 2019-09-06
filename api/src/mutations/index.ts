ccd6cabe458a8391999c0a408201e609bee0d889
Sanjeev Kumar Pandit <sanjeevpandit@lftechnology.com>
2019-09-07 05:43:43 +0545

    SPORTS-193 Rename file

--- api/src/mutations/index.ts
+++ api/src/mutations/index.ts
@@ -2,7 +2,7 @@ import { login } from './login';
 import { signUp } from './signUp';
 import { createGame, editGame, deleteGame } from './game';
 import { refreshAccessToken } from './refreshAccessToken';
-import { createRound, editRound, deleteRound } from './rounds';
+import { createRound, editRound, deleteRound } from './round';
 import { createCategory, editCategory, deleteCategory } from './category';
 
 export default {
