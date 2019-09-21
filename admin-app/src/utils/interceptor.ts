714bfec0033388b0fa5db1fa7e5b7569b72ddfa3
sagar acharya <sagarach65@gmail.com>
2019-09-21 20:18:25 +0545

    SPORTS-238 Add TODO block

--- admin-app/src/utils/interceptor.ts
+++ admin-app/src/utils/interceptor.ts
@@ -170,6 +170,7 @@ export default function setup() {
         }
 
         if (code === HttpStatusCodes.FORBIDDEN) {
+          // TODO: Logout from the application here
           clearUserSession();
         }
 
